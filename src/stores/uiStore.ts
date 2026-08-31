952a742dedc3f352e66913a78d26af752808b021
Mario <andresmart92@gmail.com>
2026-08-31 10:10:19 -0600

    Creates state to track selected date for hourly forecast

--- src/stores/uiStore.ts
+++ src/stores/uiStore.ts
@@ -3,10 +3,14 @@ import { create } from "zustand";
 interface UiStore {
   changeUnitSystem: (unitType: "metric" | "imperial") => void;
   unitSystem: "metric" | "imperial";
+  selectedDay: string;
+  setSelectedDay: (value: string) => void;
 }
 
 const useUiStore = create<UiStore>((set) => ({
   unitSystem: "imperial",
+  selectedDay: "",
+  setSelectedDay: (value: string) => set({ selectedDay: value }),
   changeUnitSystem: (unitType: "metric" | "imperial") => set({ unitSystem: unitType }),
 }));
 
