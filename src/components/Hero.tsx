858c35630e4b8bca80e2a05c4d23bff6da5e840a
Mario <andresmart92@gmail.com>
2026-09-01 13:55:43 -0600

    Creates hero to display location/current temp

---
+++ src/components/Hero.tsx
@@ -0,0 +1,31 @@
+import moment from "moment";
+
+import useWeatherStore from "../stores/weatherStore";
+import useUiStore from "../stores/uiStore";
+import Card from "./Card";
+import { getIconURL } from "../utils/utils";
+
+const Hero: React.FC = () => {
+  const { data } = useWeatherStore();
+  const { unitSystem } = useUiStore();
+
+  if (!data) return null;
+
+  const currentTemp = unitSystem === "metric" ? data.current.feelslike_c : data.current.feelslike_f;
+  const iconURL = getIconURL(data.current.condition.code);
+
+  return (
+    <Card className="flex flex-col items-center justify-between text-center px-6 py-20 gap-6 md:flex-row md:text-left relative hero">
+      <div>
+        <h2 className="text-[1.75rem] font-bold">{data.location.name}, {data.location.region}</h2>
+        <h4 className="text-[1.125rem]">{moment(data.location.localtime).format("dddd, MMMM d, YYYY")}</h4>
+      </div>
+      <div className="flex gap-4">
+        <img src={iconURL} alt={data.current.condition.text} width={80} height={80} />
+        <h1 className="text-8xl"><span className="italic">{currentTemp}</span> °</h1>
+      </div>
+    </Card>
+  );
+};
+
+export default Hero;
