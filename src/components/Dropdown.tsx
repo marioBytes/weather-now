c411cfe61c5c863bbf4df06ef53b6a547e18aee1
Mario <andresmart92@gmail.com>
2026-09-02 15:11:03 -0600

    Close dropdown if user clicks away

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 import DropdownItem from "./DropdownItem";
 import IconDropdown from "../assets/iconDropdown";
@@ -18,8 +18,37 @@ interface DropdownProps {
   showCheckmark?: boolean;
 }
 
-const Dropdown: React.FC<DropdownProps> = ({ buttonText, options, value, onChange, multi = false, showCheckmark = false }) => {
+const Dropdown: React.FC<DropdownProps> = ({
+  buttonText,
+  options,
+  value,
+  onChange,
+  multi = false,
+  showCheckmark = false,
+}) => {
   const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen]);
+
+  const handleOnChange = (value: string, isDisabled = false) => {
+    if (isDisabled) return;
 
   const handleOnChange = (value: string) => {
     onChange(value);
@@ -38,7 +67,10 @@ const Dropdown: React.FC<DropdownProps> = ({ buttonText, options, value, onChang
         </span>
       </button>
       {isOpen && (
-        <div className="origin-top-right absolute right-0 w-56 bg-neutral-800 outline outline-neutral-600 rounded-md py-1.5 px-2 z-50 mt-2">
+        <div
+          className="origin-top-right absolute right-0 w-56 bg-neutral-800 outline outline-neutral-600 rounded-md py-1.5 px-2 z-50 mt-2"
+          ref={dropdownRef}
+        >
           {options.map((option) => {
             const isSelected = option.value === value || value.includes(option.value);
 
