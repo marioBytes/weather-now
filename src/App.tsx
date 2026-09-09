5c73a1c6539e003734bef16ee2074132e475d2b8
Mario <andresmart92@gmail.com>
2026-09-09 14:19:54 -0600

    Full re-render of page would reset state of search component

--- src/App.tsx
+++ src/App.tsx
@@ -27,18 +27,6 @@ function App() {
     }
   }, [getGeolocation, geolocation, fetchForecast]);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
-  if (!data) {
-    return <div>No data</div>;
-  }
-
   return (
     <div className="grid grid-cols-12 gap-8 p-4 md:p-6 xl:p-2">
       <div className="col-span-12">
@@ -96,6 +84,27 @@ function App() {
       <div className="col-span-12 lg:col-start-4 lg:col-end-10 mb-12">
         <Search />
       </div>
+      {error && <div>{error}</div>}
+      {loading && <div>loading</div>}
+      {!loading && data && (
+        <>
+          <div className="flex flex-col gap-8 col-span-12 xl:col-span-8">
+            <div className="flex flex-col gap-8">
+              <Hero />
+              <CurrentWeatherStack />
+            </div>
+            <div className="flex flex-col gap-4">
+              <h3 className="text-3xl">Daily forecast</h3>
+              <DailyForecastStack />
+            </div>
+          </div>
+          <div className="col-span-12 xl:col-span-4 xl:relative">
+            <div className="xl:absolute xl:inset-0">
+              <HourlyForecast />
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
