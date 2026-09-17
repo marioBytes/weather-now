c4b0ae8217f053c666c05fb51fbba08ac2b048fa
Mario <andresmart92@gmail.com>
2026-09-17 09:15:38 -0600

    Loading state for hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,26 +6,36 @@ import Card from "./Card";
 import { getIconURL } from "../utils/utils";
 
 const Hero: React.FC = () => {
-  const { data } = useWeatherStore();
+  const { data, loading } = useWeatherStore();
   const { units } = useUiStore();
 
-  if (!data) return null;
-
-  const currentTemp = units.temp === "c" ? data.current.feelslike_c : data.current.feelslike_f;
-  const iconURL = getIconURL(data.current.condition.code);
+  const currentTemp = data ? (units.temp === "c" ? data?.current.feelslike_c : data?.current.feelslike_f) : "";
+  const iconURL = data ? getIconURL(data.current.condition.code) : "";
 
   return (
-    <Card className="flex flex-col items-center justify-between text-center px-6 py-20 gap-6 md:flex-row md:text-left relative hero">
-      <div>
-        <h2 className="text-[1.75rem] font-bold">{data.location.name}, {data.location.country}</h2>
-        <h4 className="text-[1.125rem]">{moment(data.location.localtime).format("dddd, MMMM D, YYYY")}</h4>
-      </div>
-      <div className="flex gap-4">
-        <img src={iconURL} alt={data.current.condition.text} width={80} height={80} className="self-center" />
-        <h1 className="text-8xl">
-          <span className="italic">{currentTemp}°</span>
-        </h1>
-      </div>
+    <Card
+      className={`flex flex-col items-center text-center px-6 py-20 gap-6 md:flex-row md:text-left${
+        loading ? " justify-center" : " justify-between"
+      }`}
+    >
+      {loading ? (
+        <h4 className="text-[1.125rem] text-neutral-200">Loading...</h4>
+      ) : (
+        <>
+          <div>
+            <h2 className="text-[1.75rem] font-bold">
+              {data?.location.name}, {data?.location.country}
+            </h2>
+            <h4 className="text-[1.125rem]">{moment(data?.location.localtime).format("dddd, MMMM D, YYYY")}</h4>
+          </div>
+          <div className="flex gap-4">
+            <img src={iconURL} alt={data?.current.condition.text} width={80} height={80} className="self-center" />
+            <h1 className="text-8xl">
+              <span className="italic">{currentTemp}°</span>
+            </h1>
+          </div>
+        </>
+      )}
     </Card>
   );
 };
