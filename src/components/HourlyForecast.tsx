824d6adc31d8dd057f36c60972acfffce00f5692
Mario <andresmart92@gmail.com>
2026-09-17 09:17:58 -0600

    Loading state for hourly forecast
    Include current hour forecast. Edgecase where selected day would be empty bc it's currenlty the last hour of the day

--- src/components/HourlyForecast.tsx
+++ src/components/HourlyForecast.tsx
@@ -9,15 +9,22 @@ import Card from "./Card";
 import Dropdown from "./Dropdown";
 
 const HourlyForecast: React.FC = () => {
-  const { data } = useWeatherStore();
+  const { data, loading } = useWeatherStore();
   const { selectedDay, setSelectedDay, units } = useUiStore();
 
-  const options = data!.forecast!.forecastday.map((forecast) => {
-    const date = moment(forecast.date).format("YYYY-MM-DD");
-    return { field: moment(forecast.date).format("dddd"), value: date };
-  });
+  const options = loading
+    ? [{ field: "-", value: "" }]
+    : data!.forecast!.forecastday.map((forecast) => {
+        const date = moment(forecast.date).format("YYYY-MM-DD");
+        return { field: moment(forecast.date).format("dddd"), value: date };
+      });
 
   const localtime = data?.location.localtime;
+  const loadingItems: number[] = [];
+
+  for (let i = 0; i < 6; i++) {
+    loadingItems.push(i);
+  }
 
   return (
     <div className="bg-neutral-800 rounded-3xl overflow-hidden xl:h-full h-120">
@@ -25,44 +32,73 @@ const HourlyForecast: React.FC = () => {
         <div className="flex items-center justify-between pb-4">
           <h3 className="text-[1.25rem]">Hourly forecast</h3>
           <Dropdown
-            buttonText={moment(selectedDay).format("dddd")}
+            buttonText={loading ? "-" : moment(selectedDay).format("dddd")}
             options={options}
             onChange={(value) => setSelectedDay(value)}
             value={selectedDay}
+            disabled={loading}
           />
         </div>
         <div className="flex flex-col gap-4">
-          {data!
-            .forecast!.forecastday.find((day) => moment(day.date).isSame(selectedDay))
-            ?.hour.map((hour) => {
-              if (
-                moment(selectedDay).date() === moment(localtime).date() &&
-                moment(hour.time).hour() <= moment(localtime).hour()
-              ) {
-                return;
-              }
+          {loading && loadingItems.map((i) => <ForecastCard key={i} loading />)}
+          {!loading &&
+            data &&
+            data
+              .forecast!.forecastday.find((day) => moment(day.date).isSame(selectedDay))
+              ?.hour.filter(
+                (hour) =>
+                  !(
+                    moment(selectedDay).date() === moment(localtime).date() &&
+                    moment(hour.time).hour() < moment(localtime).hour()
+                  ),
+              )
+              .map((hour) => {
+                const time = moment(hour?.time).format("h A");
+                const iconURL = getIconURL(hour.condition.code);
+                const temp = units.temp === "f" ? hour.temp_f : hour.temp_c;
 
-              const time = moment(hour.time).format("h A");
-              const iconURL = getIconURL(hour.condition.code);
-              const temp = units.temp === "f" ? hour.temp_f : hour.temp_c;
-
-              return (
-                <Card key={hour.time} bg="700">
-                  <div className="flex items-center justify-between">
-                    <div className="flex items-center gap-2">
-                      <img src={iconURL} alt={hour.condition.text} width={40} height={40} />
-                      <h4 className="text-[1.25rem]">{time}</h4>
-                    </div>
-                    <div>
-                      <h4 className="text-[1.25rem]">{temp}°</h4>
-                    </div>
-                  </div>
-                </Card>
-              );
-            })}
+                return (
+                  <ForecastCard
+                    key={time}
+                    alt={hour.condition.text}
+                    iconURL={iconURL}
+                    temp={temp}
+                    time={time}
+                    loading={false}
+                  />
+                );
+              })}
         </div>
       </div>
     </div>
+  );
+};
+
+interface ForecastCardProps {
+  loading: boolean;
+  alt?: string;
+  iconURL?: string;
+  temp?: number;
+  time?: string;
+}
+
+const ForecastCard: React.FC<ForecastCardProps> = ({ alt, iconURL, loading, temp, time }) => {
+  return (
+    <Card bg="700">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          {loading ? (
+            <div className="h-5"></div>
+          ) : (
+            <>
+              <img src={iconURL} alt={alt} height={40} width={40} />
+              <h4 className="text-[1.25rem]">{time}</h4>
+            </>
+          )}
+        </div>
+        <div>{loading ? <div className="h-5"></div> : <h4 className="text-[1.25rem]">{temp}°</h4>}</div>
+      </div>
+    </Card>
   );
 };
 
