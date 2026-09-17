19942fa9b9e6deefcaae0c72ab849cbc82c3d3f2
Mario <andresmart92@gmail.com>
2026-09-17 09:14:32 -0600

    Loading state for current weather card

--- src/components/CurrentWeatherCard.tsx
+++ src/components/CurrentWeatherCard.tsx
@@ -1,16 +1,20 @@
 import Card from "./Card";
 
+import useWeatherStore from "../stores/weatherStore";
+
 interface CurrentWeatherCardProps {
   title: string;
   value: string;
 }
 
 const CurrentWeatherCard: React.FC<CurrentWeatherCardProps> = ({ title, value }) => {
+  const { loading } = useWeatherStore();
+
   return (
     <Card className="col-span-3 md:col-span-3">
       <div className="flex flex-col gap-4">
         <h3 className="text-xl text-neutral-300">{title}</h3>
-        <h2 className="text-3xl font-light">{value}</h2>
+        <h2 className="text-3xl font-light">{loading ? "-" : value}</h2>
       </div>
     </Card>
   );
