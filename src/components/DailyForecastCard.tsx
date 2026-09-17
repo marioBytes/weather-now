751b20e95b03ccfefebef7d75245fad37bb36767
Mario <andresmart92@gmail.com>
2026-09-17 09:15:21 -0600

    Loading state for daily forecast

--- src/components/DailyForecastCard.tsx
+++ src/components/DailyForecastCard.tsx
@@ -2,6 +2,7 @@ import Card from "./Card";
 import moment from "moment";
 
 import { getIconURL } from "../utils/utils";
+import useWeatherStore from "../stores/weatherStore";
 
 interface DailyForecastCardProps {
   date: string;
@@ -11,18 +12,29 @@ interface DailyForecastCardProps {
 }
 
 const DailyForecastCard: React.FC<DailyForecastCardProps> = ({ date, code, low, high }) => {
+  const { loading } = useWeatherStore();
   const iconUrl = getIconURL(code);
   const dayOfWeek = moment(date).format("ddd");
 
   return (
     <Card className="px-2.5 py-4">
       <div className="flex flex-col gap-4 items-center">
-        <h3 className="text-lg font-dm-sans">{dayOfWeek}</h3>
-        <img className="text-center" src={iconUrl} alt="icon" height={60} width={60} />
-        <div className="flex justify-between w-full">
-          <p>{high}°</p>
-          <p>{low}°</p>
-        </div>
+        {loading ? <div className="h-4"></div> : <h3 className="text-lg font-dm-sans">{dayOfWeek}</h3>}
+        {loading ? (
+          <div className="h-15"></div>
+        ) : (
+          <img className="text-center" src={iconUrl} alt="icon" height={60} width={60} />
+        )}
+        {loading ? (
+          <div>
+            <div className="h-4"></div>
+          </div>
+        ) : (
+          <div className="flex justify-between w-full">
+            <p>{high}°</p>
+            <p>{low}°</p>
+          </div>
+        )}
       </div>
     </Card>
   );
